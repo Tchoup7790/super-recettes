0ab502e0d24d2bee2a9e6590b0dbe8b9da4c926a
clementmarius <106822014+clementmarius@users.noreply.github.com>
2025-04-10 12:07:18 +0200

    dao

---
+++ src/main/java/com/group/SuperRecipes/model/dao/Ingredient.java
@@ -0,0 +1,11 @@
+package com.group.SuperRecipes.model.Ingredient;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.annotations.UuidGenerator;
