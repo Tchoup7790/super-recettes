b341b870a1b8f0fc93254f274bfe7b6f0145b076
unknown <maxime.robin44@yahoo.fr>
2025-04-10 10:29:56 +0200

    add controller Category

---
+++ src/main/java/com/group/SuperRecipes/controller/CategoryController.java
@@ -0,0 +1,39 @@
+package com.group.SuperRecipes.controller;
+import com.group.SuperRecipes.model.Category;
+import com.group.SuperRecipes.service.CategoryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+// src/main/java/com/group/SuperRecipes/controller/CategoryController.java
+@RestController
+@RequestMapping("/categories")
+public class CategoryController {
+    private final CategoryService service;
+
+    public CategoryController(CategoryService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Category> all() {
+        return service.findAll();
+    }
+
+    @PostMapping
+    public Category create(@RequestBody Category category) {
+        return service.save(category);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Category> one(@PathVariable Long id) {
+        return service.findById(id)
+            .map(ResponseEntity::ok)
+            .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
