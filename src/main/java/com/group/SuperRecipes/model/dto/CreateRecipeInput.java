cbccc64e0c2dd522b218784c5ac49ab6cdf31916
unknown <maxime.robin44@yahoo.fr>
2025-04-11 11:11:26 +0200

    Add Step Entity

--- src/main/java/com/group/SuperRecipes/model/dto/CreateRecipeInput.java
+++ src/main/java/com/group/SuperRecipes/model/dto/CreateRecipeInput.java
@@ -1,10 +1,12 @@
 package com.group.SuperRecipes.model.dto;
 
+import java.util.List;
 import java.util.Set;
 
 public record CreateRecipeInput(
         String title,
         Integer preparationTime,
         String categoryId,
-        Set<String> ingredientIds) {
+        Set<String> ingredientIds,
+        List<CreateStepInput> steps) {
 }
